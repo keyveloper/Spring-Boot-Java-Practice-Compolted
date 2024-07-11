e962dec520d4db029d4be8978cdd57a748ba10a0
jword <didehdrb21@naver.com>
2024-07-11 18:32:03 +0900

    EntityManger - JPQL

--- src/main/java/com/example/webserver/DataBaseModel.java
+++ src/main/java/com/example/webserver/DataBaseModel.java
@@ -1,131 +1,85 @@
 package com.example.webserver;
 
-import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+
+import jakarta.persistence.*;
 import java.sql.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @Getter
 @Repository
 @Slf4j
 public class DataBaseModel {
-    private HikariDataSource dataSource;
+    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myPU");
 
-    @PostConstruct
-    public void startConnection() {
-        log.info("Starting database connection with HikariCP");
-
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl("jdbc:mariadb://localhost:3306/http");
-        hikariConfig.setUsername("root");
-        hikariConfig.setPassword("3353");
-        hikariConfig.setDriverClassName("org.mariadb.jdbc.Driver");
-        hikariConfig.setMaximumPoolSize(10);
-        hikariConfig.setMinimumIdle(10);
-        hikariConfig.setConnectionTimeout(20000);
-        hikariConfig.setIdleTimeout(300000);
-        hikariConfig.setMaxLifetime(600000);
-
-        this.dataSource = new HikariDataSource(hikariConfig);
-
-        try (Connection connection = dataSource.getConnection()) {
-            log.info("Database connection established successfully");
-        } catch (SQLException e) {
-            log.error("Failed to establish database connection: " + e.getMessage());
-        }
-    }
     public Optional<String> putText(String key, String value) {
-        String sql = "INSERT INTO texts (text_id, text_value) VALUES ?, ?";
+        try(EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            TextEntity textEntity = new TextEntity();
+            textEntity.setTextId(key);
+            textEntity.setTextValue(value);
+            entityManager.getTransaction().begin();
+            entityManager.persist(textEntity);
+            entityManager.getTransaction().commit();
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, key);
-            preparedStatement.setString(2, value);
-
-            int affectedRows = preparedStatement.executeUpdate();
-
-            // Check if any rows were affected.
-            if (affectedRows > 0) {
-                log.info("A new row has been inserted successfully.");
-                return Optional.of("insertion successful");  // Return true if the insertion was successful.
-            } else {
-                log.error("A new row insertion failed");
-                return Optional.empty();  // Return false if the insertion failed.
-            }
-        } catch (SQLException e) {
-            log.error("DataBase Manger: putText err: " + e.getMessage());
+            return Optional.of("Insertion successful");
+        } catch (Exception e) {
+            log.error(e.getMessage());
             return Optional.empty();
         }
     }
-
 
     public Optional<String> getText(String key) {
-        String sql = "SELECT text_value FROM texts WHERE text_id = ?";
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql);
-             ResultSet resultSet = preparedStatement.executeQuery()){
-            preparedStatement.setString(1, key);
-
-            if (resultSet.next()) {
-                return Optional.of(resultSet.getString(1));
-            }
-        } catch (SQLException e) {
-            log.error("DataBase Manger: getText err: " + e.getMessage());
+        try(EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            TextEntity textEntity = entityManager
+                    .createQuery("SELECT t FROM TextEntity t Where t.textId = :key", TextEntity.class)
+                    .setParameter("key", key)
+                    .getSingleResult();
+            return Optional.ofNullable(textEntity.getTextValue());
+        } catch (NoResultException e) {
+            log.error("get Text result error" + e.getMessage());
             return Optional.empty();
         }
-        return Optional.empty();
     }
 
     public Optional<HashMap<String, String>> getTextAll() {
         // return Json
         String sql = "SELECT * FROM texts";
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql);
-             ResultSet resultSet = preparedStatement.executeQuery()){
-
-            HashMap<String, String > results = new HashMap<>();
-            while (resultSet.next()) {
-                String key = resultSet.getString(1);
-                String value = resultSet.getString(2);
-                results.put(key, value);
-            }
-
-            if (results.isEmpty()) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            List<TextEntity> textEntities = entityManager
+                    .createQuery("SELECT t From TextEntity t", TextEntity.class)
+                    .getResultList();
+            if (textEntities.isEmpty()) {
                 return Optional.empty();
-            } else {
-                return Optional.of(results);
             }
-        } catch (SQLException e) {
-            log.error("Database Manager: getTextAll error: " + e.getMessage());
-            return Optional.empty();
+
+            HashMap<String, String> textMap = new HashMap<>();
+            for (TextEntity text : textEntities) {
+                textMap.put(text.getTextId(), text.getTextValue());
+            }
+
+            return Optional.of(textMap);
         }
     }
 
 
     public Optional<String> deleteText(String key) {
-        String sql = "DELETE FROM texts WHERE text_id = (?)";
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql);){
-            preparedStatement.setString(1, key);
-
-            int effectedRows = preparedStatement.executeUpdate();
-            if (effectedRows > 0) {
-                log.info("key: " + key + " row was deleted!");
-                return Optional.of("key: " + key + " row was deleted!");
-            } else {
-                log.info("delete failed");
-                return Optional.empty();
-            }
-        } catch (SQLException e) {
-            log.error("DataBase Manger: deleteText err: " + e.getMessage());
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            entityManager.getTransaction().begin();
+            TextEntity textEntity = entityManager
+                    .createQuery("SELECT t From TextEntity t Where t.textId = :textId", TextEntity.class)
+                    .setParameter("textId", key)
+                    .getSingleResult();
+            entityManager.remove(textEntity);
+            entityManager.getTransaction().commit();
+            return Optional.of("Key: " + key + " row was deleted!");
+        } catch (Exception e) {
+            // EntityManager 자체가 닫히면 모든 열려 있는 트랜잭션은 자동 롤백된다.
+            log.error("DataBase Model: delete error: " + e.getMessage());
             return Optional.empty();
         }
     }
--- src/main/java/com/example/webserver/TextEntity.java
+++ src/main/java/com/example/webserver/TextEntity.java
@@ -9,8 +9,11 @@ public class TextEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "text_id")
-    private long textId;
+    @Column(name = "text_num")
+    private long textNum;
+
+    @Column(name = "text_id", nullable = false)
+    private String textId;
 
     @Column(name = "text_vale", nullable = false)
     private String textValue;
