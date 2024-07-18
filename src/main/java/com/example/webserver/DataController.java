1a14c86002ac16e5c0f694a033d6187eb5aa5081
jword <didehdrb21@naver.com>
2024-07-18 15:51:55 +0900

    bulletin starts

--- src/main/java/com/example/webserver/DataController.java
+++ src/main/java/com/example/webserver/DataController.java
@@ -14,42 +14,6 @@ import java.util.Optional;
 @RestController
 @AllArgsConstructor
 public class DataController {
-    DataBaseModel dataBaseModel;
 
-    @GetMapping("/time")
-    public ResponseEntity<String> getTime() {
-        LocalDateTime now = LocalDateTime.now();
-        String formattedNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-        return ResponseEntity.ok(formattedNow);
-    }
-    @GetMapping("/textall")
-    public ResponseEntity<HashMap<String, String>> getTextAll() {
-        Optional<HashMap<String, String>> texts = dataBaseModel.getTextAll();
-        if (texts.isPresent() && !texts.get().isEmpty()) {
-            return ResponseEntity.ok(texts.get());
-        }
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/text/{id}")
-    public ResponseEntity<String> getText(@PathVariable("id") String id) {
-        Optional<String> text = dataBaseModel.getText(id);
-        return text.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @PostMapping("/text/{id}")
-    public ResponseEntity<String> postText(@PathVariable("id") String id, @RequestBody String data) {
-        Optional<String> message = dataBaseModel.putText(id, data);
-        return message.map(m -> ResponseEntity.status(HttpStatus.CREATED).body(m))
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Error occurred"));
-    }
-
-    @DeleteMapping("/text/{id}")
-    public ResponseEntity<String> deleteText(@PathVariable("id") String id) {
-        Optional<String> message = dataBaseModel.deleteText(id);
-        return message.map(m -> ResponseEntity.status(HttpStatus.ACCEPTED).body(m))
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_EXTENDED).build());
-    }
 
 }
