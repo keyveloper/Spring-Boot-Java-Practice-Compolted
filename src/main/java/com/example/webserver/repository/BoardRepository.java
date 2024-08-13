a814164e2d4c32c0e790a449ea2be6a42d8780d9
omen <didehdrb21@naver.com>
2024-08-13 17:21:11 +0900

    QueryDSL dependecies added

--- src/main/java/com/example/webserver/repository/BoardRepository.java
+++ src/main/java/com/example/webserver/repository/BoardRepository.java
@@ -3,7 +3,6 @@ package com.example.webserver.repository;
 import com.example.webserver.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
